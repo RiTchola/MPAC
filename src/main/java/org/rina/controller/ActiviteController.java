7176d8009e597566adda9e3f4b71cf9cb1626c77
Rina T <yseukam@yahoo.com>
2023-09-22 00:55:10 +0200

    Activité ok et commenté

--- src/main/java/org/rina/controller/ActiviteController.java
+++ src/main/java/org/rina/controller/ActiviteController.java
@@ -1,7 +1,6 @@
 package org.rina.controller;
 
 import org.rina.controller.exceptions.NotExistException;
-
 import org.rina.dto.request.ActiviteDto;
 import org.rina.model.Activite;
 import org.rina.model.Etablissement;
@@ -10,9 +9,7 @@ import org.rina.service.EtablissementServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import jakarta.validation.Valid;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -20,85 +17,95 @@ import java.util.Optional;
 @RequestMapping("/activite")
 public class ActiviteController {
 
-	@Autowired
-	private ActiviteServices activiteService;
-	@Autowired
-	private EtablissementServices etablissementService;
+    @Autowired
+    private ActiviteServices activiteService;
+    @Autowired
+    private EtablissementServices etablissementService;
 
-	/**
+    /**
      * Récupérer tous les activités.
      */
-	@GetMapping
-	public ResponseEntity<List<Activite>> getAllActivity() {
-		List<Activite> activities = activiteService.findAll();
-		return ResponseEntity.ok(activities);
-	}
+    @GetMapping
+    public ResponseEntity<List<Activite>> getAllActivity() {
+        // Récupère la liste de toutes les activités
+        List<Activite> activities = activiteService.findAll();
+        // Renvoie la liste des activités en réponse
+        return ResponseEntity.ok(activities);
+    }
 
-	/**
+    /**
      * Récupérer une activité par son ID.
      */
-	@GetMapping("/{id}")
-	public ResponseEntity<Activite> getActivityById(@PathVariable Long id) {
-		Optional<Activite> activite = activiteService.findById(id);
-		if (activite.isPresent()) {
-			return ResponseEntity.ok(activite.get());
-		}
+    @GetMapping("/{id}")
+    public ResponseEntity<Activite> getActivityById(@PathVariable Long id) {
+        // Recherche une activité par son ID
+        Optional<Activite> activite = activiteService.findById(id);
+        // Si l'activité existe, renvoie-la en réponse, sinon renvoie une réponse 404 (non trouvée)
+        if (activite.isPresent()) {
+            return ResponseEntity.ok(activite.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
-		else return ResponseEntity.notFound().build();
-	}
-
-	 /**
+    /**
      * Créer une nouvelle activité.
      */
-	@PostMapping
-	public ResponseEntity<Activite> createActivity(@Valid @RequestBody ActiviteDto actDto) {
-		Long idEtab = Long.valueOf(1);
-		Etablissement etab = etablissementService.findById(idEtab)
-				.orElseThrow(() -> new NotExistException(idEtab.toString()));
+    @PostMapping
+    public ResponseEntity<Activite> createActivity(@Valid @RequestBody ActiviteDto actDto) {
+        // Obtient l'établissement associé à l'activité
+        Long idEtab = Long.valueOf(1);
+        Etablissement etab = etablissementService.findById(idEtab)
+                .orElseThrow(() -> new NotExistException(idEtab.toString()));
 
-		// Crée et insère l'activité
-		Activite newActivite = actDto.toActivite(etab);
-		activiteService.insert(newActivite);
-		
-		// Renvoie la réponse avec le activité créé et l'ID généré
+        // Crée une nouvelle activité à partir des données de la requête
+        Activite newActivite = actDto.toActivite(etab);
+        // Insère la nouvelle activité dans la base de données
+        activiteService.insert(newActivite);
+        // Renvoie une réponse avec la nouvelle activité créée et l'ID généré
         return ResponseEntity.ok(newActivite);
-	}
+    }
 
-	 /**
+    /**
      * Mettre à jour une activité existante.
      */
-	@PutMapping("/{id}")
-	public ResponseEntity<Activite> updateActivity(@PathVariable Long id, @Valid @RequestBody ActiviteDto actDto) {
-		// Vérifie d'abord si l'activité existe en fonction de l'ID
-	    Optional<Activite> existingActivite = activiteService.findById(id);
-	    
-	    if (existingActivite.isPresent()) {
-	    	Long idEtab = Long.valueOf(1);
-			Etablissement etab = etablissementService.findById(idEtab)
-					.orElseThrow(() -> new NotExistException(idEtab.toString()));
+    @PutMapping("/{id}")
+    public ResponseEntity<Activite> updateActivity(@PathVariable Long id, @Valid @RequestBody ActiviteDto actDto) {
+        // Vérifie si l'activité existe en fonction de l'ID
+        Optional<Activite> existingActivite = activiteService.findById(id);
 
-			// Mise à jour l'activité existante avec les nouvelles valeurs
-	        actDto.setId(id);
-	        Activite updateActivite = activiteService.updateActivite(id, actDto.toActivite(etab));
-	        
-	        //Renvoie la réponse avec le activité mis à jour
-	        return ResponseEntity.ok(updateActivite);
-	    }
-	    
-	    else return ResponseEntity.notFound().build();	
-	}
+        if (existingActivite.isPresent()) {
+            // Obtient l'établissement associé à l'activité
+            Long idEtab = Long.valueOf(1);
+            Etablissement etab = etablissementService.findById(idEtab)
+                    .orElseThrow(() -> new NotExistException(idEtab.toString()));
 
-	/**
+            // Met à jour l'activité existante avec les nouvelles valeurs
+            actDto.setId(id);
+            Activite updateActivite = activiteService.updateActivite(id, actDto.toActivite(etab));
+            
+            // Renvoie une réponse avec l'activité mise à jour
+            return ResponseEntity.ok(updateActivite);
+        } else {
+            // Si l'activité n'existe pas, renvoie une réponse 404 (non trouvée)
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    /**
      * Supprimer une activité par son ID.
      */
-	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> deleteActivity(@PathVariable Long id) {
-		if (activiteService.existsById(id)) {
-			activiteService.deleteById(id);
-			return ResponseEntity.ok().build();
-		}
-		
-		else return ResponseEntity.notFound().build();
-	}
-	
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteActivity(@PathVariable Long id) {
+        // Vérifie si l'activité existe en fonction de l'ID
+        if (activiteService.existsById(id)) {
+            // Supprime l'activité si elle existe
+            activiteService.deleteById(id);
+            // Renvoie une réponse 200 (OK) pour indiquer que l'activité a été supprimée avec succès
+            return ResponseEntity.ok().build();
+        } else {
+            // Si l'activité n'existe pas, renvoie une réponse 404 (non trouvée)
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- src/main/java/org/rina/model/Activite.java
+++ src/main/java/org/rina/model/Activite.java
@@ -8,44 +8,42 @@ import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
-@Data
-@Builder
-@NoArgsConstructor
-@Entity
-@Table(name = "TACTIVITE")
+@Data 
+@Builder // Génère un constructeur de type Builder
+@NoArgsConstructor 
+@Entity 
+@Table(name = "TACTIVITE") 
 public class Activite {
-	
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
     private Long id;
     
-    @Column(length = 50, nullable = false)
-	private String nom;
+    @Column(length = 50, nullable = false) // Spécifie les propriétés de la colonne nom
+    private String nom;
 
-	@Column(nullable = false)
-	private Date date;
+    @Column(nullable = false) // Spécifie les propriétés de la colonne date
+    private Date date;
 
-	/**
-	 * jointure à d'autres classes
-	 */
-	@ManyToOne
-	@JoinColumn(name = "FKETABLISSEMENT", nullable = false)
-	private Etablissement etablissement;
+    /**
+     * jointure à d'autres classes
+     */
+    @ManyToOne // Indique qu'il s'agit d'une relation Many-to-One
+    @JoinColumn(name = "FKETABLISSEMENT", nullable = false) 
+    private Etablissement etablissement;
 
-	/**
-	 * Construction
-	 *
-	 * @param id
-	 * @param nom
-	 * @param date
-	 * @param etablissement
-	 */
-	public Activite(Long id, String nom, Date date, Etablissement etablissement) {
+    /**
+     * Constructeur avec des arguments 
+     * @param id L'identifiant de l'activité
+     * @param nom Le nom de l'activité 
+     * @param date La date de l'activité 
+     * @param etablissement L'établissement lié à cette activité 
+     */
+    public Activite(Long id, String nom, Date date, Etablissement etablissement) {
 
-		this.id = id;
-		this.nom = nom;
-		this.date = date;
-		this.etablissement = etablissement;
-	}
-
+        this.id = id;
+        this.nom = nom;
+        this.date = date;
+        this.etablissement = etablissement;
+    }
 }
