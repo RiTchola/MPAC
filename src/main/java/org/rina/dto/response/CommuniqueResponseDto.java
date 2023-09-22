412b11bd8f7250e9fb147cfc63cf5cf8c48f846a
Rina T <yseukam@yahoo.com>
2023-09-22 11:01:49 +0200

    établissement ok et commenté

---
+++ src/main/java/org/rina/dto/response/CommuniqueResponseDto.java
@@ -0,0 +1,32 @@
+package org.rina.dto.response;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.rina.model.Communique;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CommuniqueResponseDto {
+
+    private Long id;
+
+    private LocalDate date;
+
+    private String titre;
+
+    private String contenu;
+
+    private List<String> fileURL;
+
+    public CommuniqueResponseDto(Communique communique) {
+        this.id = communique.getId();
+        this.date = communique.getDate();
+        this.titre = communique.getTitre();
+        this.contenu = communique.getContenu();
+        this.fileURL = communique.getFileURL();
+    }
+}
