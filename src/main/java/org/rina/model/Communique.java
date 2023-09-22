0f9ae9f10c710d3d87c05a864d72a69d8b925569
Rina T <yseukam@yahoo.com>
2023-09-22 08:51:07 +0200

    Communiqué ok et commenté

--- src/main/java/org/rina/model/Communique.java
+++ src/main/java/org/rina/model/Communique.java
@@ -1,6 +1,7 @@
 package org.rina.model;
 
 import java.time.LocalDate;
+
 import java.util.List;
 
 import jakarta.persistence.Column;
@@ -22,48 +23,48 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "TCOMMUNIQUE")
 public class Communique {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     @Column(nullable = false)
-	private LocalDate date;
+    private LocalDate date;
 
-	@Column(nullable = false)
-	private String titre;
+    @Column(nullable = false)
+    private String titre;
 
-	@Column(nullable = false)
-	private String contenu;
+    @Column(nullable = false)
+    private String contenu;
 
-	@ElementCollection
-	@Column(nullable = true)
-	private List<String> fileURL;
+    @ElementCollection
+    @Column(nullable = true)
+    private List<String> fileURL;
 
-	/**
-	 * jointure à d'autres classes
-	 */
+    /**
+     * Jointure à d'autres classes
+     */
+    @ManyToOne
+    @JoinColumn(name = "FKEtablissement", nullable = false)
+    private Etablissement etablissement;
 
-	@ManyToOne
-	@JoinColumn(name = "FKEtablissement", nullable = false)
-	private Etablissement etablissement;
+    /**
+     * Construction
+     * @param id L'identifiant du communique
+     * @param date La date du communique 
+     * @param titre Le titre du communique 
+     * @param contenu Le contenu du communique 
+     * @param fileURL La liste des chemins de fichiers associés au communique 
+     * @param etablissement L'établissement lié à ce communique 
+     */
+    public Communique(Long id, LocalDate date, String titre, String contenu, List<String> fileURL,
+        Etablissement etablissement) {
 
-	/**
-	 * Construction
-	 * @param id
-	 * @param date
-	 * @param titre
-	 * @param contenu
-	 * @param fileURL
-	 * @param etablissement
-	 */
-	public Communique(Long id, LocalDate date, String titre, String contenu, List<String> fileURL,
-			Etablissement etablissement) {
-
-		this.id = id;
-		this.date = date;
-		this.titre = titre;
-		this.contenu = contenu;
-		this.fileURL = fileURL;
-		this.etablissement = etablissement;
-	}
+        this.id = id;
+        this.date = date;
+        this.titre = titre;
+        this.contenu = contenu;
+        this.fileURL = fileURL;
+        this.etablissement = etablissement;
+    }
 }
